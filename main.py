1d2bb24761ad77a1fcfee0054c9c3b3018d03bcc
Omer Meister <stifmayster@yahoo.com>
2023-05-17 18:08:46 +0300

    click method added

--- main.py
+++ main.py
@@ -3,19 +3,22 @@ import math
 
 pi = math.pi
 
-def circle_hekef(r):
 
-    return abs(r) * 2 * pi +88
+def circle_hekef(r):
+    return abs(r) * 2 * pi + 88
 
 
 def circle_area(r):
     return r * r * pi
 
+
 def now():
     return datetime.datetime.now()
+
+
+def click():
+    print("Click")
 
 print("lo amarti cushi")
 print("lo amarti joe joe")
 print(now())
-
-
